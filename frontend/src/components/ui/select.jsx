27e3923127ca918ca1080dae6dc99a610bae62d9
jeewani56 <malshikajeewani486@gmail.com>
2026-03-21 14:35:00 +0530

    update

--- frontend/src/components/ui/select.jsx
+++ frontend/src/components/ui/select.jsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { cn } from "../../lib/utils"
+import { cn } from "@/lib/utils"
 
 const Select = ({ children, onValueChange, defaultValue }) => {
   const [value, setValue] = React.useState(defaultValue)
